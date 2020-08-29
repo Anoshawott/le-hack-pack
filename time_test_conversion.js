db9073aa004554f5e3dd1069b089bc24a14ef353
Anosh Samsudeen <54537931+Anoshawott@users.noreply.github.com>
2020-08-29 13:32:18 +1000

    Update time_test_conversion.js

--- time_test_conversion.js
+++ time_test_conversion.js
@@ -1,5 +1,5 @@
-var startDate1 = new Date("12:30PM");
-var startDate2 = new Date("12:50AM");
-var startDate3 = new Date("15:40PM");
+var startDate1 = new Date("12:30");
+var startDate2 = new Date("12:50");
+var startDate3 = new Date("15:40");
 alert(startDate1.getTime() > startDate2.getTime()); // false
 alert(startDate2.getTime() == startDate3.getTime()); //false
